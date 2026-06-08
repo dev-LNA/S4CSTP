3495d354245c3361e500a4106fd89f5b3f40044b
Denis <denis.bernardes099@gmail.com>
2026-06-08 14:58:37 -0300

    refactore read_log.py

--- scripts/read_log.py
+++ scripts/read_log.py
@@ -15,6 +15,7 @@ Usage:
 """
 
 import configparser
+import getpass
 import logging
 import os
 import smtplib
@@ -25,49 +26,50 @@ from email.mime.text import MIMEText
 from os.path import join
 from pathlib import Path
 
-from dotenv import load_dotenv
+import dotenv
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-base_folder = join(cwd, *3 * [".."], "SPARC4", "ACS")
+
 logging.basicConfig(
     level=logging.INFO,
     filename=join(cwd, "log.log"),
     filemode="w",
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-logging.info(f"The python version used in this run is {sys.executable}")
-# --------- Read CFG file ---------------
-config = configparser.ConfigParser()
-cfg_file = join(base_folder, "acs_config.cfg")
-try:
-    config.read(cfg_file)
-    logging.info(f"The file {cfg_file} has been read.")
-except FileNotFoundError as e:
-    logging.info(f"The {cfg_file} file was not found." + repr(e))
+logging.info(f"The python interpreter used in this run is {sys.executable}")
 
+
+# --------- Read CFG file ---------------
+username = getpass.getuser()
+config_file = Path(f"C:/Users/{username}/SPARC4/ACS") / "acs_config.cfg"
+if not config_file.exists():
+    logging.error(f"file {config_file} not found")
+    sys.exit()
+config = configparser.ConfigParser()
+config.read(config_file)
+logging.info(f"The file {config_file} has been read.")
 channel = config.get("channel configuration", "channel")
 logging.info(f"This machine correspons to ACS{channel}.")
-log_folder = join(
-    Path(config.get("channel configuration", "log file path").strip(r"\""))
-)
+log_folder = config.get("channel configuration", "log file path").replace('"', "")
+log_folder = Path(log_folder)
 logging.info(f"The path in which the log files are saved is {log_folder}.")
 
 # --------- Read the log file ---------------
 yesterday = datetime.now() - timedelta(days=1)
 yesterday = yesterday.strftime("%Y%m%d")
 logging.info(f"The observation date was {yesterday}.")
-log_file = join(log_folder, f"{yesterday}_events.log")
-try:
-    with open(log_file) as file:
-        lines = file.read().splitlines()
-    logging.info("The log file has been read.")
-except FileNotFoundError as e:
-    logging.info(f"The file {yesterday}_events.log was not found." + repr(e))
+log_file = log_folder / f"{yesterday}_events.log"
+if not log_file.exists():
+    logging.error(f"file {log_file} not found")
+    sys.exit()
+with open(log_file) as file:
+    lines = file.read().splitlines()
+logging.info("The log file has been read.")
 
 BASE_STRING = f"""
 Hello,
 
-You are receiving the errors and warnings found for the SPARC4 channel {channel}, occurred in {yesterday}.
+You are receiving the errors occurred in {yesterday}, found for the SPARC4 channel {channel}.
 
 """
 EMAIL_STRING = BASE_STRING
@@ -77,34 +79,27 @@ for line in lines:
         EMAIL_STRING += line + "\n"
         i += 1
 logging.info(f"There is (are) {i} line(s) to log.")
+if i == 0:
+    logging.info("Exiting the script.")
+    sys.exit()
 
 # ------------ Send email --------------------
-load_dotenv()
+dotenv.load_dotenv()
 USER = os.getenv("GMAIL_USER")
 PASSWORD = os.getenv("GMAIL_KEY")
 RECEIVERS = [USER]
-
-
 msg = MIMEMultipart()
 msg["From"] = USER
-msg["Subject"] = (
-    f"{yesterday}: errors and warnings found for the SPARC4 channel {channel}."
-)
+msg["Subject"] = f"{yesterday}: errors found for the SPARC4 channel {channel}."
 msg.attach(MIMEText(EMAIL_STRING, "plain"))
-
-
-if EMAIL_STRING != BASE_STRING:
-    try:
-        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server.login(USER, PASSWORD)
-        texto = msg.as_string()
-        for receiver in RECEIVERS:
-            msg["To"] = receiver
-            server.sendmail(USER, receiver, texto)
-            logging.info(f"The email has been sent to {receiver} succesfully.")
-        server.quit()
-    except Exception as e:
-        logging.info(f"Error when sending the email: {repr(e)}")
-else:
-    logging.info(f"There are no events to report.")
-    logging.info(f"There are no events to report.")
+try:
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(USER, PASSWORD)
+    texto = msg.as_string()
+    for receiver in RECEIVERS:
+        msg["To"] = receiver
+        server.sendmail(USER, receiver, texto)
+        logging.info(f"The email has been sent to {receiver} succesfully.")
+    server.quit()
+except Exception as e:
+    logging.info(f"Error when sending the email: {repr(e)}")
