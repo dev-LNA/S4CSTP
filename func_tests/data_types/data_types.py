42b66d31e793bbf9baa462ced1b2420a0e72d3ff
Denis <denis.bernardes099@gmail.com>
2026-06-09 11:46:57 -0300

    remove some data types

--- func_tests/data_types/data_types.py
+++ func_tests/data_types/data_types.py
@@ -1,17 +1,7 @@
-import logging
-from dataclasses import dataclass
-from enum import Enum, Flag, IntEnum, StrEnum, auto
+from enum import Enum, StrEnum, auto
 from ipaddress import IPv4Address, IPv6Address
-from typing import Any, Container
-from unittest.mock import Base
 
-import zmq
 from pydantic import BaseModel, Field, field_validator
-
-import src.client as client
-import src.comm_protocol as comm_protocol
-import src.component.component as component
-from src.component import EACS, EICS, Fake_Component, Focuser
 
 
 class Camera_Configuration(BaseModel):
@@ -143,16 +133,6 @@ class Camera_Status(BaseModel):
         )
 
 
-class Level(Enum):
-    NONE = auto()
-    VERY_LOW = auto()
-    LOW = auto()
-    NORMAL = auto()
-    MEDIUM = auto()
-    HIGH = auto()
-    CRITICAL = auto()
-
-
 class Execution_Status(Enum):
     NONE = auto()
     IDLE = auto()
@@ -169,26 +149,7 @@ class Led_Status(StrEnum):
     WARNING = "warning"
 
 
-class Log_Level(IntEnum):  # TODO adicionar ao diagrama de classes
-    DEBUG = logging.DEBUG
-    INFO = logging.INFO
-    WARNING = logging.WARNING
-    ERROR = logging.ERROR
-    CRITICAL = logging.CRITICAL
-
-
-class Light_Sources(Flag):
-    NONE = auto()
-    DARK = auto()
-    SHUTTER = auto()
-    THORIUM = auto()
-    HALOGEN = auto()
-    CALIB = auto()
-    OBJECT = auto()
-    SKY = auto()
-
-
-class Request:
+class Test:
     def __init__(self, request_str: str) -> None:
         self._str: str = request_str
         self.__dict: dict = {}
@@ -237,60 +198,6 @@ class Request:
             self.__dict[f"field{idx + 1}"] = word
 
 
-class Command:  # TODO: add to classes diagram
-    def __init__(self, command_str: str) -> None:
-        self._str: str = command_str
-        self.__dict: dict = {}
-        self.command_len: int = 0
-        self._valid: bool = False
-        self._supported: str = "off"
-        self._timeout: str = "off"
-        self._executed: str = "off"
-
-    @property
-    def str(self) -> str:
-        return self._str
-
-    @property
-    def valid(self) -> bool:
-        return self._valid
-
-    @property
-    def _dict(self) -> dict:
-        return self.__dict
-
-    @property
-    def supported(self) -> str:
-        return self._supported
-
-    @supported.setter
-    def supported(self, val: str) -> None:
-        self._supported = val
-
-    @property
-    def timeout(self) -> str:
-        return self._timeout
-
-    @timeout.setter
-    def timeout(self, val: str) -> None:
-        self._timeout = val
-
-    @property
-    def executed(self) -> str:
-        return self._executed
-
-    @executed.setter
-    def executed(self, val: str) -> None:
-        self._executed = val
-
-    def validate(self) -> None:
-        splitted_command = self._str.split(" ")
-        self.command_len = len(splitted_command)
-        self._valid = self.command_len <= 3
-        for idx, word in enumerate(splitted_command):
-            self.__dict[f"field{idx + 1}"] = word
-
-
 class End_Point(BaseModel):
     ip: str
     port: int = Field(ge=0, le=65535)
@@ -304,168 +211,3 @@ class End_Point(BaseModel):
 
     def to_str(self) -> str:
         return f"tcp://{self.ip}:{self.port}"
-
-
-@dataclass
-class Components_Container:
-    eacs1: component.Component
-    eacs2: component.Component
-    eics: component.Component
-    focuser: component.Component
-
-    @property
-    def values(self) -> list[component.Component]:
-        return list(self.__dict__.values())
-
-    @property
-    def keys(self) -> list:
-        return [key.upper() for key in self.__dict__.keys()]
-
-    @property
-    def items(self) -> tuple:
-        return tuple(self.__dict__.items())
-
-    @property
-    def _dict(self) -> dict[str, component.Component]:
-        return {key.upper(): val for (key, val) in self.__dict__.items()}
-
-
-class Mediator_Setup:
-    def __init__(self) -> None:
-        end_point = End_Point(ip="192.168.0.1", port=5555)
-        subscriber = comm_protocol.Fake_Subscriber(end_point)
-        requester = comm_protocol.Fake_Requester(end_point)
-        eacs1 = Fake_Component(None, subscriber, requester)
-        eacs2 = Fake_Component(None, subscriber, requester)
-        eics = Fake_Component(None, subscriber, requester)
-        focuser = Fake_Component(None, subscriber, requester)
-        self._fake_container = Components_Container(
-            eacs1=eacs1,
-            eacs2=eacs2,
-            eics=eics,
-            focuser=focuser,
-        )
-        replier = comm_protocol.Fake_Replier(end_point)
-        self._fake_client = client.Client(replier)
-
-    def create(self, _type: str) -> tuple:
-        """Create a container of Component classes.
-
-        Args:
-            _type (str): container to be retrived.
-            Allowed values are 'fake', 'test_acs1', 'test_client', 'test_focs',
-            'test_acs1_client', 'test_acs1_acs2_client', 'test_all_without_eics'.
-
-        Raises:
-            ValueError: an unexpected value was found.
-
-        Returns:
-            Tuple: a tupple containing the client and container instanciated by
-            the class.
-        """
-        if _type == "fake":
-            return self._fake_client, self._fake_container
-
-        if _type == "test_acs1":
-            context = zmq.Context()
-            end_point = End_Point(ip="200.131.64.25", port=5555)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=5556)
-            requester = comm_protocol.ZeroMQ_REQ(end_point, context)
-            eacs1 = EACS(None, subscriber, requester)
-
-            fake_container = self._fake_container
-            fake_container.eacs1 = eacs1
-            return self._fake_client, fake_container
-
-        if _type == "test_client":
-            context = zmq.Context()
-            end_point = End_Point(ip="200.131.64.25", port=5557)
-            replier = comm_protocol.ZeroMQ_REP(end_point, context)
-            _client = client.Client(replier)
-
-            return _client, self._fake_container
-
-        if _type == "test_foc":
-            context = zmq.Context()
-            end_point = End_Point(ip="200.131.64.25", port=7001)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=7002)
-            requester = comm_protocol.Fake_Requester(end_point)
-            focuser = Focuser(None, subscriber, requester)
-
-            fake_container = self._fake_container
-            fake_container.focuser = focuser
-            return self._fake_client, fake_container
-
-        if _type == "test_acs1_client":
-            context = zmq.Context()
-            end_point = End_Point(ip="200.131.64.25", port=5555)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=5556)
-            requester = comm_protocol.ZeroMQ_REQ(end_point, context)
-            eacs1 = EACS(None, subscriber, requester)
-
-            end_point = End_Point(ip="200.131.64.25", port=5553)
-            replier = comm_protocol.ZeroMQ_REP(end_point, context)
-            _client = client.Client(replier)
-
-            fake_container = self._fake_container
-            fake_container.eacs1 = eacs1
-            return _client, fake_container
-
-        if _type == "test_acs1_acs2_client":
-            context = zmq.Context()
-            end_point = End_Point(ip="200.131.64.25", port=5555)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=5556)
-            requester = comm_protocol.ZeroMQ_REQ(end_point, context)
-            eacs1 = EACS(None, subscriber, requester)
-
-            end_point = End_Point(ip="200.131.64.25", port=5557)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=5558)
-            requester = comm_protocol.ZeroMQ_REQ(end_point, context)
-            eacs2 = EACS(None, subscriber, requester)
-
-            end_point = End_Point(ip="200.131.64.25", port=5559)
-            replier = comm_protocol.ZeroMQ_REP(end_point, context)
-            _client = client.Client(replier)
-
-            fake_container = self._fake_container
-            fake_container.eacs1 = eacs1
-            fake_container.eacs2 = eacs2
-            return _client, fake_container
-
-        if _type == "test_all_without_eics":
-            context = zmq.Context()
-            end_point = End_Point(ip="200.131.64.25", port=5555)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=5556)
-            requester = comm_protocol.ZeroMQ_REQ(end_point, context)
-            eacs1 = EACS(None, subscriber, requester)
-
-            end_point = End_Point(ip="200.131.64.25", port=5557)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=5558)
-            requester = comm_protocol.ZeroMQ_REQ(end_point, context)
-            eacs2 = EACS(None, subscriber, requester)
-
-            end_point = End_Point(ip="200.131.64.25", port=7001)
-            subscriber = comm_protocol.ZeroMQ_SUB(end_point, context)
-            end_point = End_Point(ip="200.131.64.25", port=7002)
-            requester = comm_protocol.Fake_Requester(end_point)
-            focuser = Focuser(None, subscriber, requester)
-
-            end_point = End_Point(ip="200.131.64.25", port=5553)
-            replier = comm_protocol.ZeroMQ_REP(end_point, context)
-            _client = client.Client(replier)
-
-            fake_container = self._fake_container
-            fake_container.eacs1 = eacs1
-            fake_container.eacs2 = eacs2
-            fake_container.focuser = focuser
-            return _client, fake_container
-
-        else:
-            raise ValueError(f"The provided type {_type} was not found.")
